4bc1c56108dc28d58b4d8bc7c8202c75b47b5b31
twoke <yangdengwei@gmail.com>
2016-10-20 20:38:49 +0800

    Create 05.py

---
+++ 05.py
@@ -0,0 +1,31 @@
+name = 'Zed A. shaw'
+age = 35
+height = 74 #inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall. " % height
+print "He's %d punds heavy." % weight
+print "Actually that's not to heavy."
+print "He's got %s eyes and %s hair." % (eyes,hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+# this line is tricky,try to get it exactly right
+print "If I add %d,%d,and %d  I get %d." % (age, height, weight, age + height + weight)
+
+# try more format characters
+my_greeting = "Hello,\t"
+my_first_name = 'Joseph'
+my_last_name = 'Pan'
+my_age = 24
+# print
+print "%r my name is %s %s, and I'm %d years old." % (my_greeting, my_first_name, my_last_name, my_age)
+
+# Try to write some variables that convert the inches and pounds to centimeters and kilos.
+inches_per_centimeters = 2.54
+pouds_per_kilo = 0.45359237
+
+print "He's %f centimeters tall." % (height * inches_per_centimeters)
+print "He's %f kilos heavy." % (weight * pouds_per_kilo)
