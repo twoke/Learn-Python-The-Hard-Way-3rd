9be77dd69489e5fb2e421b6521e3b2fe07908552
twoke <yangdengwei@gmail.com>
2016-10-21 17:37:41 +0800

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# ex10: What Was That?
+
+print "I am 6'2\" tall."  # 将字符串中的双引号转义
+print 'I am 6\'2" tall.'  # 将字符串中的单引号转义
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* cat food\n\t* Fishies\n\t* catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
