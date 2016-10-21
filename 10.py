f2feff061fb73575f5db99ea6f26787e235a3301
twoke <yangdengwei@gmail.com>
2016-10-21 18:12:24 +0800

    Update 10.py

--- 10.py
+++ 10.py
@@ -9,12 +9,27 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
+fat_cat = '''
 I'll do a list:
 \t* cat food\n\t* Fishies\n\t* catnip\n\t* Grass
-"""
+'''
 
 print tabby_cat
 print persian_cat
 print backslash_cat
 print fat_cat
+
+# Assign string value for each variable
+intro = "I'll print a week"
+mon = "Mon"
+tue = "Tue"
+wed = "Wed"
+thu = "Thu"
+fri = "Fri"
+
+print "%s\n%s\n%s\n%s\n%s\n" % (mon, tue, wed, thu, fri)
+print "%r" % intro
+print "%r" % "She said \"I'll print a week\""
+
+print "%s" % intro
+print "%s" % "She said \"I'll print a week\""
