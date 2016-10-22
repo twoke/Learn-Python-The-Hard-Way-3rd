6141c65956abf45fcfff67180085d4c93fe9298d
twoke <yangdengwei@gmail.com>
2016-10-22 12:13:40 +0800

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,21 +1,22 @@
 from sys import argv
 
-script, user_name = argv
+script, user_name city = argv
 prompt = 'Please type the answer:'
 
-print  "Hi, %s,I'm the %s script." % (user_name, script)
+print  "Hi, %s from %s,I'm the %s script." % (user_name, city, script)
 print "I'd like to ask you a few questions."
 print "Do you like me  %s?" % user_name
 likes = raw_input(prompt)
 
-print "Where do you live %s?" % user_name
-lives = raw_input(prompt)
+print "what's the weather like today in %s?" % city
+weather = raw_input(prompt)
 
 print "What kind of computer do you have?"
 computer = raw_input(prompt)
 
 print """
 Alright, so you said %r about liking me.
-you live in %r. Not sure where that is.
+The weather in your city is %s.
+But I can't feel it because I'm a robot.
 And you have a %r computer.Nice.
-""" % (lives, lives, computer)
+""" % (lives, weather, computer)
